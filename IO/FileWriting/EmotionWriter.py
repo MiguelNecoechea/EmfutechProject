bc386564eaa2008528f61e2d38bd0238750df580
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-10-17 16:11:02 +0900

    EmotionWriter V0.0.1 Initial implementation of a writer focused on the emotions recorded.

---
+++ IO/FileWriting/EmotionWriter.py
@@ -0,0 +1,23 @@
+from IO.FileWriting.Writer import Writer
+
+class EmotionPredictedWriter(Writer):
+    def __init__(self, output_path, file_name):
+        """
+        Creates a writer object for the specified that focuses on writing the emotion on each second of the experiment.
+        :param output_path: the folder where the data is going to be written.
+        :param file_name: the name of the file to be written.        """
+        super().__init__(output_path, file_name, ['Time', 'Emotion Predicted'])
+
+    def write_data(self, timestamp, data) -> bool:
+        """
+        Writes the time and the emotion predicted into a csv file.
+        :param timestamp: The timestamp of the data in form of array
+        :param data: The label containing the predicted emotion.
+        :return: True if the data was written, False otherwise.
+        """
+        written = False
+        if self._is_writer_opened and timestamp is not None and data is not None:
+            self._csv_writer.writerow([timestamp, data])
+            written = True
+
+        return written
