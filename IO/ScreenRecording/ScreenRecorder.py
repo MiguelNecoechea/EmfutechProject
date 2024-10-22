5a3c9c7679942179cd0a03e762eb65804c0f8097
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-10-22 10:35:37 +0900

    Added documentation, removed destructor.

--- IO/ScreenRecording/ScreenRecorder.py
+++ IO/ScreenRecording/ScreenRecorder.py
@@ -1,6 +1,10 @@
 from abc import abstractmethod
 
 class ScreenRecorder:
+    """
+    An abstract class containing the base implementation for a screen recorder.
+    Specific OS implementations should be in a different file.
+    """
     def __init__(self, path_to_save, filename):
         self._path = path_to_save
         self._filename = filename
@@ -12,6 +16,3 @@ class ScreenRecorder:
     @abstractmethod
     def stop_recording(self):
         pass
-
-    def __del__(self):
-        self.stop_recording()
