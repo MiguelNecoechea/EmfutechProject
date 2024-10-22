c87cebd60ab69d730fac132b8440fa80ac7cfc6a
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-10-22 10:13:33 +0900

    Removed unused imports. Remove old testing code.

--- IO/ScreenRecording/ScreenRecorder.py
+++ IO/ScreenRecording/ScreenRecorder.py
@@ -1,10 +1,4 @@
-import subprocess
-import platform
-import os
-import time
 from abc import abstractmethod
-from typing import Tuple
-
 
 class ScreenRecorder:
     def __init__(self, path_to_save, filename):
@@ -21,13 +15,3 @@ class ScreenRecorder:
 
     def __del__(self):
         self.stop_recording()
-# "ffmpeg -f avfoundation -i \"3\" output.mp4"
-# ffmpeg -f avfoundation -list_devices true -i ""
-# subprocess.run(["ffmpeg", "-f", "avfoundation", "-list_devices", "true", "-i", "\"\""])
-# subprocess.run(["ffmpeg", "-f", "avfoundation", "-i", "3", "output1.mp4"])
-#
-# for i in range(14):
-#     print(i)
-#     time.sleep(1)
-#
-# subprocess.run(["q"])
