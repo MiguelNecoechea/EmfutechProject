c1bdb461687c0a92b781bde34ff46d925781e7c5
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-10-24 17:26:19 +0900

    Removed unnecessary wrapper.

--- IO/EyeTracking/main.py
+++ IO/EyeTracking/main.py
@@ -1,15 +1,18 @@
-from LaserGaze.LaserGaze import LaserGaze
+from LaserGaze.GazeProcessor import GazeProcessor
+from LaserGaze.VisualizationOptions import VisualizationOptions
 import cv2
-laser_gaze = LaserGaze()
-laser_gaze.start_camera_and_processor()
+vo = VisualizationOptions()
+laser_gaze = GazeProcessor(visualization_options=vo)
+laser_gaze.start()
 
 while True:
-    data = laser_gaze.get_gaze_vector_and_frame()
+    data = laser_gaze.get_gaze_vector()
     if data is not None:
         frame = data[-1]
+        print("left: ", data[0], " right: ",data[1])
         cv2.imshow('LaserGaze', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-laser_gaze.stop()
+laser_gaze.stop_processing()
