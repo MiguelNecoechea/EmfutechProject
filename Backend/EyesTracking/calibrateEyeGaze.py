a1d6284e4e4c73578cf14d4d15dc4479109719b3
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-10-30 18:05:12 +0900

    Added more components to be able to control LaserGaze.

--- Backend/EyesTracking/calibrateEyeGaze.py
+++ Backend/EyesTracking/calibrateEyeGaze.py
@@ -8,37 +8,62 @@
 import sys
 import os
 
+
 # Add the parent directory of 'IO' to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 import eel
-
+from IO.FileWriting.GazeWriter import GazeWriter
 from IO.EyeTracking.LaserGaze.GazeProcessor import GazeProcessor
 
+_gaze_processor = GazeProcessor()
+_data_writer = GazeWriter('data', 'gaze_data.csv')
+_current_x_coordinate = 0
+_current_y_coordinate = 0
+
 @eel.expose
-def get_coordinates(x, y):
+def set_coordinates(x, y):
     """
     Recibe y almacena las coordenadas actuales del punto de calibración
     """
-    print(f"Punto de calibración: X={x}, Y={y}")
-    return x, y
+    _current_x_coordinate = x
+    _current_y_coordinate = y
+    print(f"Punto de calibración: X={_current_x_coordinate}, Y={_current_y_coordinate}")
+    if _gaze_processor.running:
+        gaze_vector = _gaze_processor.get_gaze_vector()
+        print(f"left: {gaze_vector[0]}, right: {gaze_vector[1]}")
+
+    else:
+        print("I am closed")
+
 
 @eel.expose
 def start_eye_gaze():
     """
     Starts the eye gaze tracking
     """
-    print("Iniciando seguimiento de la mirada")
-    gaze_processor = GazeProcessor()
-    gaze_processor.start()
+    _gaze_processor.start()
     while True:
-        gaze_data = gaze_processor.get_gaze_vector()
+        gaze_data = _gaze_processor.get_gaze_vector()
+        print(f"left: {gaze_data[0]}, right: {gaze_data[1]}")
         if gaze_data[0] is not None and gaze_data[1] is not None:
             break
+
+@eel.expose
+def stop_eye_gaze():
+    _gaze_processor.stop_processing()
+
+
+def write_gaze_data():
+    data = _gaze_processor.get_gaze_vector()
+
+    # _data_writer.write_gaze_data(data)
 
 def main():
     print("Iniciando servidor de Eye Tracking...")
     base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    _data_writer.create_new_file()
+
     eel.init(os.path.join(base_dir, 'Frontend'))
     try:
         print("Starting Eel server...")
