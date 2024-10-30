61fa9d9b84581e1f7640a65ee5eaa2c5c9933207
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-10-30 09:47:33 +0900

    Fixed the function to get coordinates.

--- Backend/EyesTracking/EyesTrackingRecorded.py
+++ Backend/EyesTracking/EyesTrackingRecorded.py
@@ -6,27 +6,26 @@
 
 import eel
 import os
-from datetime import datetime
+
 class EyeTrackingRecorder:
     def __init__(self):
         self.current_coordinates = None
         self.coordinates_history = []
-        
-    @eel.expose
-    def get_coordinates(self, x, y):
-        """
-        Recibe y almacena las coordenadas actuales del punto de calibración
-        """
-        self.current_coordinates = (x, y)
-        self.coordinates_history.append((x, y))
-        print(f"Punto de calibración: X={x}, Y={y}")
-        return x, y
-    
+
     def get_current_point(self):
         """
         Retorna las coordenadas actuales del punto de calibración
         """
         return self.current_coordinates
+
+@eel.expose
+def get_coordinates(x, y):
+    """
+    Recibe y almacena las coordenadas actuales del punto de calibración
+    """
+    print(f"Punto de calibración: X={x}, Y={y}")
+    return x, y
+
 
 def init_eel():
     base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
