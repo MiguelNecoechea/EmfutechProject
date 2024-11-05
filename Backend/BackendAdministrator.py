627168eb7e74f531d7f617c607b3c3c31103c3e2
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-11-05 16:48:32 +0900

    Beta version. Still in development.

--- Backend/BackendAdministrator.py
+++ Backend/BackendAdministrator.py
@@ -1,20 +1,18 @@
-from mne_lsl.stream import StreamLSL as Stream
+import time
 
+from mne_lsl.stream import StreamLSL as Stream
 from IO.FileWriting.AuraDataWriter import AuraDataWriter
 from IO.SignalProcessing.AuraTools import rename_aura_channels, is_stream_ready
 
+import threading
+
 finished_data_collection = False
 
-def handle_aura_signal(stream_id, buffer_size_multiplier):
-    stream = Stream(bufsize=buffer_size_multiplier, source_id=stream_id)
-    stream.connect(processing_flags='all')
-    rename_aura_channels(stream)
-    data_writer = AuraDataWriter('test', 'test.csv')
-    data_writer.create_new_file()
+def _data_collection_loop(stream, data_writer):
     while True:
         if is_stream_ready(stream):
-           data, ts = stream.get_data()
-           data_writer.write_data(ts, data)
+            data, ts = stream.get_data()
+            data_writer.write_data(ts, data)
 
         if finished_data_collection:
             break
@@ -24,4 +22,21 @@ def handle_aura_signal(stream_id, buffer_size_multiplier):
     del data_writer
     del stream
 
-handle_aura_signal('filtered', 1)
+def handle_aura_signal(stream_id, buffer_size_multiplier, output_path='.', file_name='aura_data.csv'):
+    stream = Stream(bufsize=buffer_size_multiplier, source_id=stream_id)
+    stream.connect(processing_flags='all')
+    rename_aura_channels(stream)
+    channels = ['timestamp'] + stream.info['ch_names']
+    data_writer = AuraDataWriter(output_path, file_name, channels)
+    data_writer.create_new_file()
+
+    data_thread = threading.Thread(target=_data_collection_loop, args=(stream, data_writer))
+    data_thread.start()
+
+
+handle_aura_signal('AURA_Power', 1)
+print("Hello World")
+for i in range(5):
+    print(i)
+    time.sleep(1)
+finished_data_collection = True
