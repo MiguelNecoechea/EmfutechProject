63fcf955640b74f2dba15e44eeb99cfc0219a4e2
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-11-06 00:18:55 +0900

    Added code for the handle of emotions. Added testing code.

--- Backend/BackendAdministrator.py
+++ Backend/BackendAdministrator.py
@@ -2,12 +2,23 @@ import time
 
 from mne_lsl.stream import StreamLSL as Stream
 from IO.FileWriting.AuraDataWriter import AuraDataWriter
+from IO.FileWriting.EmotionWriter import EmotionPredictedWriter
 from IO.SignalProcessing.AuraTools import rename_aura_channels, is_stream_ready
 
 import threading
 
+from IO.VideoProcessing.EmotionRecognizer import EmotionRecognizer
+
+__OUT_TESTING_PATH = '/Users/mnecoea/PycharmProjects/AuraSignalProcessing/TestingOutput'
 finished_data_collection = False
 
+# Global Stopping function
+def stop_data_collection():
+    global finished_data_collection
+    finished_data_collection = True
+
+
+# Thread specific functions to collect data using other threads.
 def _data_collection_loop(stream, data_writer):
     while True:
         if is_stream_ready(stream):
@@ -22,6 +33,23 @@ def _data_collection_loop(stream, data_writer):
     del data_writer
     del stream
 
+def _emotion_collection_loop(emotion_handler, emotion_writer):
+    while True:
+        emotion = emotion_handler.recognize_emotion()
+        if emotion is not None:
+            emotion = emotion[0]['dominant_emotion']
+        else:
+            emotion = 'Undefined'
+        emotion_writer.write_data(time.time(), emotion)
+
+        if finished_data_collection:
+            break
+
+    emotion_writer.close_file()
+    del emotion_writer
+    del emotion_handler
+
+# Data specific handler for each type of "Signal" to be collected.
 def handle_aura_signal(stream_id, buffer_size_multiplier, output_path='.', file_name='aura_data.csv'):
     stream = Stream(bufsize=buffer_size_multiplier, source_id=stream_id)
     stream.connect(processing_flags='all')
@@ -33,10 +61,24 @@ def handle_aura_signal(stream_id, buffer_size_multiplier, output_path='.', file_
     data_thread = threading.Thread(target=_data_collection_loop, args=(stream, data_writer))
     data_thread.start()
 
+def handle_emotion(output_path='.', file_name='emotions.csv'):
+    emotion_handler = EmotionRecognizer('opencv')
+    emotion_writer = EmotionPredictedWriter(output_path, file_name)
+    emotion_writer.create_new_file()
 
-handle_aura_signal('AURA_Power', 1)
-print("Hello World")
-for i in range(5):
-    print(i)
-    time.sleep(1)
-finished_data_collection = True
+    emotion_thread = threading.Thread(target=_emotion_collection_loop, args=(emotion_handler, emotion_writer))
+    emotion_thread.start()
+
+
+# Main function.
+def main():
+    global finished_data_collection
+    handle_aura_signal('AURA_Power', 1, __OUT_TESTING_PATH, 'aura_data.csv')
+    handle_emotion(__OUT_TESTING_PATH)
+    for i in range(5):
+        print(i)
+        time.sleep(1)
+    finished_data_collection = True
+
+if __name__ == '__main__':
+    main()
