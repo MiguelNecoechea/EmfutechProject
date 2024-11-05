e6cfc8366ff8e67cceede6ef1c37efa209275be4
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-11-05 16:48:40 +0900

    Testing File.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 
 import matplotlib.pyplot as plt
 
-from IO.ScreenRecording.WindowsScreenRecorder import WindowsScreenRecorder
+# from IO.ScreenRecording.WindowsScreenRecorder import WindowsScreenRecorder
 from IO.SignalProcessing.AuraSignalHandler import AuraLslStreamHandler
 
 # handler = VideoHandler()
@@ -39,44 +39,49 @@ from IO.SignalProcessing.AuraSignalHandler import AuraLslStreamHandler
 #
 # del lsl_writer
 # del emotion_writer
-name = 'AURA'
-lowbp = 1.0
-highbp = 50.0
-channel = 'AuraLSL-20241025-12;10;20'
-stream = AuraLslStreamHandler(1, channel)
-print(stream.available_streams())
-stream.drop_channels(['P4', 'P3', 'C4', 'Pz', 'C3'])
+# name = 'AURA'
+# lowbp = 1.0
+# highbp = 50.0
+# channel = 'AuraLSL-20241025-12;10;20'
+# stream = AuraLslStreamHandler(1, channel)
+# print(stream.available_streams())
+# stream.drop_channels(['P4', 'P3', 'C4', 'Pz', 'C3'])
 
-stream.add_notch_filter()
-stream.add_filter(lowbp, highbp)
-
-stream.clear_buffer()
-f, ax = plt.subplots(3, 1, sharex=True, constrained_layout=True)
-for _ in range(3):  # acquire 3 separate window
-    # figure how many new samples are available, in seconds
-    winsize = stream.get_stream_new_samples() / stream.get_stream_frequency()
-    # retrieve and plot data
-    while not stream.is_stream_ready():
-        pass
-    else:
-        data, ts = stream.get_data_from_stream()
-        print(data)
-        for k, data_channel in enumerate(data):
-            if k >= 3:
-                break
-            ax[k].plot(ts, data_channel)
-ax[-1].set_xlabel("Timestamp (LSL time)")
-plt.show()
-print(stream.get_stream_info())
-
-recorder = WindowsScreenRecorder('tast', "test")
-recorder.start_recording()
-for i in range(10):
-    time.sleep(1)
-
-recorder.stop_recording()
+# stream.add_notch_filter()
+# stream.add_filter(lowbp, highbp)
+#
+# stream.clear_buffer()
+# f, ax = plt.subplots(3, 1, sharex=True, constrained_layout=True)
+# for _ in range(3):  # acquire 3 separate window
+#     # figure how many new samples are available, in seconds
+#     winsize = stream.get_stream_new_samples() / stream.get_stream_frequency()
+#     # retrieve and plot data
+#     while not stream.is_stream_ready():
+#         pass
+#     else:
+#         data, ts = stream.get_data_from_stream()
+#         print(data)
+#         for k, data_channel in enumerate(data):
+#             if k >= 3:
+#                 break
+#             ax[k].plot(ts, data_channel)
+# ax[-1].set_xlabel("Timestamp (LSL time)")
+# plt.show()
+# print(stream.get_stream_info())
+#
+# recorder = WindowsScreenRecorder('tast', "test")
+# recorder.start_recording()
+# for i in range(10):
+#     time.sleep(1)
+#
+# recorder.stop_recording()
 # from screeninfo import screeninfo, Monitor
 #
 # monitors = screeninfo.get_monitors()
 #
 # print(monitors)
+
+
+
+
+# channel = 'AURA_Power'
