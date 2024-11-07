f72d796df0a9229b7aee3c59c2862e6a3ee71504
Miguel Necoechea <necoecheamiguel@gmail.com>
2024-11-07 23:23:06 +0900

    Removed old unsafe implementation.

--- Backend/EyeGaze.py
+++ Backend/EyeGaze.py
@@ -36,132 +36,50 @@ Run this script to start the Eye Tracking server and expose the functions to the
 import sys
 import os
 import time
+import asyncio
 import threading
 
 # Add the parent directory of 'IO' to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from IO.FileWriting.GazeWriter import GazeWriter
 from IO.EyeTracking.LaserGaze.GazeProcessor import GazeProcessor
-from Backend.EyeCoordinateRegressor import PositionRegressor
 
-_gaze_processor = GazeProcessor()
-__trining_data_writer = GazeWriter('data', 'training_data.csv')
-_current_x_coordinate = 0
-_current_y_coordinate = 0
-_recording_data = False
-_regressor = None
-_regressor_running = False
 
-# Functions exposed to the web interface
-def set_coordinates(x, y):
+def create_new_eye_gaze() -> GazeProcessor:
     """
-    Receives and stores the current calibration point coordinates. the coordinates are retrieved from the javascript
-    generator of points.
-
-    Args:
-        x (int): The x-coordinate of the calibration point.
-        y (int): The y-coordinate of the calibration point.
+    Attempts to create a working instance of the GazeProcessor class. If the instance cannot be created, it will retry
+    until it is successful. To achieve this, the object will be deleted and recreated if the gaze data is not valid.
+    This function can take a while to complete, as it waits for the gaze data to be valid.
     """
-    global _current_y_coordinate
-    global _current_x_coordinate
-    _current_x_coordinate = x
-    _current_y_coordinate = y
-
-def start_eye_gaze():
-    """
-    Starts the eye gaze tracking and attempts to perform the internal calibration of the gaze processor.
-    When the calibration is unsuccessful, the gaze processor is restarted by deleting it from memory and creating a new
-    instance.
-    """
-    global _gaze_processor
-    print("Starting eye gaze tracking...")
-    _gaze_processor.start()
+    gaze_processor = GazeProcessor()
+    gaze_processor.start()
 
     while True:
-        gaze_data = _gaze_processor.get_gaze_vector()
+        gaze_data = gaze_processor.get_gaze_vector()
         if gaze_data[0] is not None and gaze_data[1] is not None:
-            break
+            return gaze_processor
         elif (gaze_data[0] is None and gaze_data[1] is not None) or (gaze_data[0] is not None and gaze_data[1] is None):
-            _gaze_processor.stop_processing()
-            del _gaze_processor
+            gaze_processor.stop_processing()
+            del gaze_processor
             time.sleep(1)
-            _gaze_processor = GazeProcessor()
-            _gaze_processor.start()
+            gaze_processor = GazeProcessor()
+            gaze_processor.start()
 
-def stop_eye_gaze():
+def create_regressor():
     """
-    Stops the eye gaze tracking process. This function should be called when the experiment is over or the user wants to
-    stop the gaze tracking.
+    Creates a regressor object that can be used to predict the user's gaze based on the training data. The regressor
+    object also will be trained on the collected data before being returned.
+    :return A PositionRegressor object that is ready for making predictions.
     """
-    _gaze_processor.stop_processing()
+    raise NotImplementedError("This function is not implemented yet.")
 
-def start_recording_training_data():
-    """
-    Starts recording the gaze data in a separate thread. The recording will continue until stop_recording is called.
-    :return:
-    """
-    global _recording_data
-    _recording_data = True
-    recording_thread = threading.Thread(target=record_gaze_training_data)
-    recording_thread.start()
 
-def stop_recording_training_data():
-    """
-    Stops recording the gaze data and closes the data file. This function should be called when the training is over.
-    :return:
-    """
-    global _recording_data
-    _recording_data = False
-    __trining_data_writer.close_file()
-
-def start_regressor():
-    """
-    Starts the regressor thread. This function is called when the user wants to start the predictions of the eye position
-    based on the gaze vector. When the final version is up this function will be called when the experiment begins.
-    :return:
-    """
-    global _regressor
-    global _regressor_running
-    _regressor = PositionRegressor('data/training_data.csv')
-    _regressor_running = True
-    _regressor.train_create_model()
-
-def stop_regression():
-    """
-    Stops the regressor thread. by setting a boolean to false.
-    """
-    global _regressor_running
-    _regressor_running = False
-
-# Python only functions
-def record_gaze_training_data():
-    """
-    Continuously records gaze data while _recording_data is True. This function is supposed to run in a separate thread.
-    :return:
-    """
-    global _recording_data
-    while True:
-        write_gaze_traing_data()
-        if not _recording_data:
-            break
-
-def write_gaze_traing_data():
-    """
-    Writes the current gaze data and calibration point to the data file. This uses the Writing classes from the IO
-    module.
-    """
-    data = _gaze_processor.get_gaze_vector()
-    if data[0] is not None and data[1] is not None:
-        combined_data = list(data[0]) + list(data[1]) + [_current_x_coordinate, _current_y_coordinate]
-        __trining_data_writer.write(combined_data)
-
-def make_prediction():
-    if _regressor is None:
-        raise RuntimeError("Regressor not trained yet")
-    gaze_data = _gaze_processor.get_gaze_vector()
-    if gaze_data[0] is not None and gaze_data[1] is not None:
-        data = [[gaze_data[0][0], gaze_data[0][1], gaze_data[0][2], gaze_data[1][0], gaze_data[1][1], gaze_data[1][2]]]
-        result = _regressor.make_prediction(data)
-        return result
-    return None
+# def write_gaze_traing_data():
+#     """
+#     Writes the current gaze data and calibration point to the data file. This uses the Writing classes from the IO
+#     module.
+#     """
+#     data = _gaze_processor.get_gaze_vector()
+#     if data[0] is not None and data[1] is not None:
+#         combined_data = list(data[0]) + list(data[1]) + [_current_x_coordinate, _current_y_coordinate]
+#         __trining_data_writer.write(combined_data)
